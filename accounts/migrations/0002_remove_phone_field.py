1a16e5e5592ab2327eeaae6a05224289619f9ef2
dennis <denniswu@wsgfo.com>
2025-04-02 16:45:47 +0800

    移除 CustomUser 模型中的 phone 欄位

---
+++ accounts/migrations/0002_remove_phone_field.py
@@ -0,0 +1,14 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='customuser',
+            name='phone',
+        ),
+    ] 
--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,6 @@ from django.db import models
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    phone = models.CharField(max_length=15, blank=True, verbose_name='電話')
     company = models.CharField(max_length=100, blank=True, verbose_name='公司')
     department = models.CharField(max_length=100, blank=True, verbose_name='部門')
     position = models.CharField(max_length=100, blank=True, verbose_name='職位')
