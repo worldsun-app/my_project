70ac0ecfbfede53e6f10b7088973380c47b7f063
dennis <denniswu@wsgfo.com>
2025-04-02 17:43:01 +0800

    修復 CustomUser 模型的必填字段問題

--- my_project/settings.py
+++ my_project/settings.py
@@ -270,7 +270,10 @@ def create_superuser():
         User.objects.create_superuser(
             username=DJANGO_SUPERUSER_USERNAME,
             email=DJANGO_SUPERUSER_EMAIL,
-            password=DJANGO_SUPERUSER_PASSWORD
+            password=DJANGO_SUPERUSER_PASSWORD,
+            phone_number='',  # 添加空字符串作為默認值
+            first_name='',    # 添加空字符串作為默認值
+            last_name=''      # 添加空字符串作為默認值
         )
         print(f"超級用戶 {DJANGO_SUPERUSER_USERNAME} 已創建")
     else:
