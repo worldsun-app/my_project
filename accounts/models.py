7129714f9c4cc8ad6fa7601833dac20f183cfa09
dennis <denniswu@wsgfo.com>
2025-04-02 17:36:33 +0800

    簡化 CustomUser 模型，移除不必要的字段

--- accounts/models.py
+++ accounts/models.py
@@ -4,10 +4,6 @@ from django.db import models
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    company = models.CharField(max_length=100, blank=True, verbose_name='公司')
-    department = models.CharField(max_length=100, blank=True, verbose_name='部門')
-    position = models.CharField(max_length=100, blank=True, verbose_name='職位')
-    
     class Meta:
         verbose_name = '用戶'
         verbose_name_plural = '用戶'
