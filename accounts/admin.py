090251a042f602fe3b10f3c7fb32ff23ce759c67
dennis <denniswu@wsgfo.com>
2025-04-07 10:09:11 +0800

    修改 CustomUserAdmin，移除不再存在的字段

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,17 +2,16 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
-    list_filter = ('is_staff', 'is_active', 'groups')
-    search_fields = ('username', 'first_name', 'last_name', 'email')
+    list_display = ('username', 'email', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('username', 'email')
     ordering = ('username',)
     
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        ('個人資料', {'fields': ('first_name', 'last_name', 'email', 'phone', 'company', 'department', 'position')}),
-        ('權限', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('个人信息', {'fields': ('email',)}),
+        ('权限', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('重要日期', {'fields': ('last_login', 'date_joined')}),
     )
     
@@ -22,3 +21,5 @@ class CustomUserAdmin(UserAdmin):
             'fields': ('username', 'email', 'password1', 'password2'),
         }),
     )
+
+admin.site.register(CustomUser, CustomUserAdmin)
