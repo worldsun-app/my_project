7e6486c50807d60e144dc401c7a8ad56ef6b5916
dennis <denniswu@wsgfo.com>
2025-04-09 14:18:12 +0800

    添加投資文件下載的URL模式

--- documents/urls.py
+++ documents/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('insurance/<int:pk>/download/', views.download_insurance_document, name='insurance_document_download'),
     path('investment/<str:category>/', views.investment_document_list, name='investment_documents_list'),
     path('investment/<int:pk>/', views.investment_document_detail, name='investment_document_detail'),
+    path('investment/<int:pk>/download/', views.download_investment_document, name='investment_document_download'),
     path('investment/create/', views.InvestmentDocumentCreateView.as_view(), name='investment_document_create'),
     path('investment/<int:pk>/update/', views.InvestmentDocumentUpdateView.as_view(), name='investment_document_update'),
     path('investment/<int:pk>/delete/', views.InvestmentDocumentDeleteView.as_view(), name='investment_document_delete'),
