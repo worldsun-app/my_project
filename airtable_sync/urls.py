d27ac60b741d7e9778c86de412b00a90fb387ddd
dennis <denniswu@wsgfo.com>
2025-04-11 13:22:08 +0800

    移除不存在的 sync_documents 視圖函數

--- airtable_sync/urls.py
+++ airtable_sync/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('airtable-documents/', views.AirtableDocumentListView.as_view(), name='airtable_document_list'),
     path('airtable-documents/<str:document_id>/download/', views.download_document, name='download_document'),
-    path('airtable-documents/sync/', views.sync_documents, name='sync_documents'),
 ] 
