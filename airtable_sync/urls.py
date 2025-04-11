29dbeb5dd6aed38ff5ed61cb36ec882e37254091
dennis <denniswu@wsgfo.com>
2025-04-11 13:37:50 +0800

    添加 airtable_sync 命名空間

--- airtable_sync/urls.py
+++ airtable_sync/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'airtable_sync'
+
 urlpatterns = [
     path('airtable-documents/', views.AirtableDocumentListView.as_view(), name='airtable_document_list'),
     path('airtable-documents/<str:document_id>/download/', views.download_document, name='download_document'),
