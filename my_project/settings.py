f71e454309432a5795a97ff0601f65e03e54363f
dennis <denniswu@wsgfo.com>
2025-04-11 11:22:53 +0800

    恢復使用 Zeabur 的 PostgreSQL 數據庫配置

--- my_project/settings.py
+++ my_project/settings.py
@@ -112,10 +112,11 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://root:nTVHdED7kehY1pBg30f5L8c9y4KoS6F2@hnd1.clusters.zeabur.com:30596/zeabur',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # 數據庫連接池設置
