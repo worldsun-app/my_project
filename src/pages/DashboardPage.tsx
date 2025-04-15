23398118405bfe2016b5a10ccd826996b399d683
dennis <denniswu@wsgfo.com>
2025-04-15 11:28:51 +0800

    優化布局：調整文件列表寬度並讓預覽區域自適應填充剩餘空間

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -304,7 +304,7 @@ const DashboardPage: React.FC = () => {
       {/* 主要內容區域 */}
       <div className="flex-1 flex min-w-0">
         {/* 中間文件列表 */}
-        <div className="w-[180px] flex-shrink-0 bg-white border-r border-gray-200">
+        <div className="w-[280px] flex-shrink-0 bg-white border-r border-gray-200">
           <div className="p-4">
             {/* 搜索框 */}
             <div className="mb-4">
@@ -392,7 +392,7 @@ const DashboardPage: React.FC = () => {
         </div>
 
         {/* 右側預覽區 */}
-        <div className="w-1/2 flex flex-col min-w-0 bg-white">
+        <div className="flex-1 flex flex-col min-w-0 bg-white">
           {selectedFile ? (
             <>
               {/* 預覽區頂部 */}
