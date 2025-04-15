f580eab37ea2f1d13936781faa70010b577a87e7
dennis <denniswu@wsgfo.com>
2025-04-15 09:45:15 +0800

    修改布局結構：使用固定寬度和 flex-1 確保正確的寬度比例

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -302,9 +302,9 @@ const DashboardPage: React.FC = () => {
       </div>
 
       {/* 主要內容區域 */}
-      <div className="grid grid-cols-12 flex-1">
+      <div className="flex-1 flex">
         {/* 中間文件列表 */}
-        <div className="col-span-4 bg-white overflow-y-auto border-r border-gray-200">
+        <div className="w-[400px] bg-white overflow-y-auto border-r border-gray-200">
           <div className="p-4">
             {/* 搜索框 */}
             <div className="mb-4">
@@ -391,7 +391,7 @@ const DashboardPage: React.FC = () => {
           </div>
         </div>
         {/* 右側預覽區 */}
-        <div className="col-span-8 bg-white overflow-y-auto">
+        <div className="flex-1 bg-white overflow-y-auto">
           {selectedFile ? (
             <div className="h-full flex flex-col">
               {/* 預覽區頂部 */}
